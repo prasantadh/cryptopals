1f17d7d9e9e1de468ada56f74d9757f6f1717fbd
Prasant Adhikari <pa1038@nyu.edu>
2018-07-08 00:15:59 +0400

    updated to use crypto library

--- set1/challenge2.py
+++ set1/challenge2.py
@@ -1,13 +1,11 @@
+from Crypto.Util.strxor import strxor
 from codecs import encode, decode
-from struct import pack
 
-# get the hex
 s1 = input('Enter the first hex: ')
 s2 = input('Enter the second hex: ')
-
-# assert correct input
-assert(len(s1) == len(s2))
-assert(len(s1) % 2 == 0)
-
-# print out the results
-print(hex(int(s1, 16) ^ int(s2, 16))[2:])
+s1 = decode(s1, 'hex')
+s2 = decode(s2, 'hex')
+ans = strxor(s1, s2).hex()
+# alternatively,
+# ans = hex(int(s1, 16) ^ int(s2, 16))[2:]
+print(ans)
