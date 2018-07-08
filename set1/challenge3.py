1c64d81f9402271cfc540ecb9dc4e18eae60b394
Prasant Adhikari <pa1038@nyu.edu>
2018-07-08 17:03:16 +0400

    updated to use lambda

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -1,36 +1,14 @@
 from codecs import encode, decode
 from Crypto.Util.strxor import strxor_c
-# ETAOIN
-def score(table):
-    ans = 0
-    for s in 'ETAOINetaoin':
-        if s in table.keys():
-            ans += table[s]
-    return ans
 
-def find_key(ct):
-    max_score = 0
-    for k in range(127):
-        table = {}
-        for b in ct:
-            if chr(k ^ b) in table:
-                table[chr(k ^ b)] += 1
-            else:
-                table[chr(k ^ b)] = 1
-        if score(table) > max_score:
-            max_score, key = score(table), k
-    return key
-    
-def decrypt(key, ct):
-    pt = ''
-    for c in ct:
-        pt += chr(c ^ key)
-    return pt
+def single_byte_xor(ct):
+    score = lambda s: sum([1 for c in s.lower() if c in b'etaoin '])
+    return max([strxor_c(ct, k) for k in range(256)], key=score)
 
 def main():
     ct = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
     ct = decode(ct, 'hex')
-    print(decrypt(find_key(ct), ct))
+    print(single_byte_xor(ct).hex())
 
 
 if __name__ == '__main__':
