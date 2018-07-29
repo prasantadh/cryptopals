1a806ee5faf6f3f6168ebc84deadaef13be2d4e2
Prasant Adhikari <pa1038@nyu.edu>
2018-07-29 20:06:11 +0400

    solved matasano challenge 7

---
+++ challenge7.py
@@ -0,0 +1,14 @@
+from codecs import encode, decode
+from Crypto.Cipher import AES
+
+def main():
+    with open('7.txt', 'r') as f:
+        ct = f.read().replace('\n', '')
+    while (len(ct) % 16): ct += '.'
+    key = b'YELLOW SUBMARINE'
+    cipher = AES.new(key, AES.MODE_ECB)
+    ct = decode(bytes(ct, 'utf-8'), 'base64')
+    print(cipher.decrypt(ct).decode('utf-8'))
+
+if __name__=='__main__':
+    main()
