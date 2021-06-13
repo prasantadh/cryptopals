538440dc739659d70eb0f4e081c547decdc358de
Prasant Adhikari <pa1038@nyu.edu>
2021-06-13 17:14:48 -0500

    Update challenge 02 to use local package

--- challenge02.py
+++ challenge02.py
@@ -1,6 +1,7 @@
 from Crypto.Util.strxor import strxor
 from codecs import encode, decode
 
+"""
 s1 = input('Enter the first hex: ')
 s2 = input('Enter the second hex: ')
 s1 = decode(s1, 'hex')
@@ -9,3 +10,10 @@ ans = strxor(s1, s2).hex()
 # alternatively,
 # ans = hex(int(s1, 16) ^ int(s2, 16))[2:]
 print(ans)
+"""
+
+from cryptopals import helpers
+ct1 = "1c0111001f010100061a024b53535009181c"
+ct2 = "686974207468652062756c6c277320657965"
+print(helpers.fixedXOR(ct1, ct2))
+
--- cryptopals/helpers.py
+++ cryptopals/helpers.py
@@ -1,12 +1,26 @@
 """
+Compute base64 encoded value of a string
 @input:
-    hexstring:
-        class: 'bytes'
-        description: a hex string
+    hexstring [string]: a hex string
 @return:
-    class: 'bytes'
-    description: base64 encoded value of hexstring
+    [bytes]: base64 encoded value of hexstring
 """
 def hex2base64(hexstring):
+
     from base64 import b64encode
     return b64encode(bytes.fromhex(hexstring))
+
+"""
+Take two equal length buffers and produce their XOR combination
+@input:
+    x [string]: a hex string
+    y [string]: a hex string
+@return:
+    [bytes]: character-wise XOR value of x and y in HEX
+"""
+def fixedXOR(x, y):
+
+    assert len(x) == len(y), "Provided strings must be of equal length"
+    return hex(int(x, 16) ^ int(y, 16))[2:]
+
+
