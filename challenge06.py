f91c6c02ec14b37d0e3543df92eead257c14cccf
Prasant Adhikari <pa1038@nyu.edu>
2021-06-13 20:28:23 -0500

    Update challenge 06 to use local package

--- challenge06.py
+++ challenge06.py
@@ -1,45 +1,46 @@
+"""
 from codecs import encode, decode
 from Crypto.Util.strxor import strxor
 from struct import pack, unpack
 from challenge3 import single_byte_xor
 from challenge5 import repeated_key_xor
+"""
 
-#number of keys to extract
-nkeys = 1
-keysize_min = 2
-keysize_max = 40
+NKEYS = 3           # number of keysizes to try # currently disabled
+KEYSIZE_MIN = 2     # minimum key length to guess
+KEYSIZE_MAX = 40    # maximum key length to guess
 
-def edit_distance(a, b):
-    n1 = lambda i : sum((i & (1 << j)) != 0 for j in range(7))
-    return sum([n1(c) for c in strxor(a, b)])
+from cryptopals import helpers
 
-def find_keysize(ct):
-    ans = []
-    for keysize in range(keysize_min, keysize_max):
-        blocks = [ct[i:i+keysize] for i in range(0, keysize*10, keysize)]
-        pairs  = [(blocks[i], blocks[j]) for i in range(len(blocks))\
-                 for j in range(i+1, len(blocks))]
-        dists  = [edit_distance(pair[0], pair[1]) for pair in pairs]
-        ans    += [(sum(dists) / keysize, keysize)]
-    return [p[1] for p in sorted(ans)[:nkeys]]
+assert helpers.edit_distance('this is a test', 'wokka wokka!!!') == 37, "Oops!"
 
-def getKey(keysize, ct):
-    key = b''
-    for i in range(keysize):
-        block = ct[i:len(ct):keysize]
-        key += single_byte_xor(block)['key']
-    return key
+with open('6.txt', 'rb') as f:
+    ciphertext = f.read().strip()
+from base64 import b64decode
+ciphertext = b64decode(ciphertext)
 
-def main():
-    assert (edit_distance(b'this is a test', b'wokka wokka!!!') == 37)
-    with open('6.txt', 'r') as f:
-        ct = f.read().replace('\n', '')
-    ct = decode(bytes(ct, 'utf-8'), 'base64')
-    for keysize in find_keysize(ct):
-        key = getKey(keysize, ct)
-        pt  = repeated_key_xor(ct, key)
-        print(key.decode('utf-8'))
-        print(pt.decode('utf-8'))
+# finding the keysize
+keysizes = []
+for keysize in range(KEYSIZE_MIN, KEYSIZE_MAX + 1):
+    distance = 0
+    for i in range(0, 20):
+        distance += helpers.edit_distance(
+                        ciphertext[i*keysize:(i+1)*keysize],
+                        ciphertext[(i+1)*keysize:(i+2)*keysize]
+                        ) / keysize
+    keysizes.append((distance, keysize))
+keysizes.sort()
+keysize = min(keysizes)[1]
 
-if __name__ == '__main__':
-    main()
+# transposing into blocks
+blocks = [''] * keysize
+for i in range(keysize):
+    blocks[i] = ciphertext[i::keysize]
+
+# contructing the key, one byte at a time
+key = ''
+for i in range(len(blocks)):
+    key += chr(helpers.brute_single_byte_xor(blocks[i], 1)[0][2])
+
+plaintext = helpers.decrypt_w_repeated_xor(ciphertext, key)
+print(plaintext.decode())
