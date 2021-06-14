ddea5297cb49ddf2c0615922dc84e01c288e5af2
Prasant Adhikari <pa1038@nyu.edu>
2021-06-13 21:57:30 -0500

    Update challenge 10 to use local package

--- challenge10.py
+++ challenge10.py
@@ -1,16 +1,22 @@
-from challenge9 import pad
+from cryptopals import helpers
 from codecs import encode, decode
 from Crypto.Util.strxor import strxor
 from Crypto.Cipher import AES
 
 def main():
+
+    # prepare the ciphertext
     with open('10.txt', 'r') as f:
         ct = f.read().replace('\n', '')
     ct = decode(bytes(ct, 'utf-8'), 'base64')
     assert(len(ct) % 16 == 0)
+
+    # prepare the cipher
     IV = b'\x00' * 16
     key = b'YELLOW SUBMARINE'
     cipher = AES.new(key, AES.MODE_ECB)
+
+    # decrypt CBC mode with ECB cipher
     blocks = [ct[i:i+16] for i in range(0, len(ct), 16)]
     pt = b''
     for block in blocks:
